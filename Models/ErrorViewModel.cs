906721305bb491bc1bd3ec15d7fced8ce9a1e498
Bruno Mota <bmotadev@gmail.com>
2022-11-17 10:46:22 -0300

    Adiciona model Tarefa
    - Adiciona model para tabela Tarefa
    - Adiciona atributos para tabela
    - Remove ErrorViewModels

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace agendadev_mvc.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
